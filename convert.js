0c627d5cdc5764742e83e7f246ac6e182ffc5e0f
Natalia Titova <bnmlucky@gmail.com>
2020-05-12 11:12:18 -0400

    rewrote 3 function declarations as arrow functions

--- convert.js
+++ convert.js
@@ -1,12 +1,9 @@
-function addToTen(num) {
-  return 10 + num;
-}
+const addToTen = num => 10 + num;
 
-function divideUs(num1, num2) {
-  return num1/num2;
-}
+const divideUs = (num1, num2) => num1 / num2;
 
-function printMyName() {
+
+const printMyName = () => {
   let myName = 'Ashley';
-	console.log(myName);
-}
+  console.log(myName);
+};
